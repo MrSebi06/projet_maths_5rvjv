4ea4969a414f58e9ce959a59771dbf3309f613ac
MrSebi06 <mihneasebastian06@gmail.com>
2025-12-02 12:58:48 +0100

    Add transform class

---
+++ include/Transform.h
@@ -0,0 +1,29 @@
+﻿//
+// Created by mihne on 02/12/2025.
+//
+
+#ifndef PROJET_MATHS_5RVJV_TRANSFORM_H
+#define PROJET_MATHS_5RVJV_TRANSFORM_H
+#include "Vector/Vector2.h"
+
+
+class Transform {
+    friend class Rigidbody2D;
+
+protected:
+    Vector2 position;
+    float rotation;
+
+public:
+    Transform(const Vector2 &position, const float rotation)
+        : position(position),
+          rotation(rotation) {}
+
+    const Vector2 &getPosition() const { return position; }
+    float getRotation() const { return rotation; }
+
+    void addPosition(const Vector2 &position_);
+};
+
+
+#endif //PROJET_MATHS_5RVJV_TRANSFORM_H
---
+++ src/Transform.cpp
@@ -0,0 +1,7 @@
+﻿//
+// Created by mihne on 02/12/2025.
+//
+
+#include "../include/Transform.h"
+
+void Transform::addPosition(const Vector2 &position_) { position += position_; }
