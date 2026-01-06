3996a912d903c18864113c0024350d5a6c8414e3
MrSebi06 <mihneasebastian06@gmail.com>
2026-01-06 19:14:30 +0100

    Add rect mesh

---
+++ include/Mesh/Polygon/Rect.h
@@ -0,0 +1,34 @@
+﻿//
+// Created by mihne on 03/12/2025.
+//
+
+#ifndef PROJET_MATHS_5RVJV_RECT_H
+#define PROJET_MATHS_5RVJV_RECT_H
+
+
+#include "Mesh/Mesh.h"
+
+class RectMesh : public Mesh {
+public:
+    float w;
+    float h;
+
+    explicit RectMesh(const float width, const float height) : w(width / 2), h(height / 2) {
+        const Vector2 vertices[4] = {
+            {w, h}, // TR
+            {w, -h}, // BR
+            {-w, -h}, // BL
+            {-w, h} // TL
+        };
+        const unsigned int indices[6] = {
+            0, 1, 2,
+            0, 2, 3
+        };
+
+        setup_geometry(vertices, 4 * sizeof(Vector2), indices,
+                       6 * sizeof(unsigned int));
+    }
+};
+
+
+#endif //PROJET_MATHS_5RVJV_RECT_H
